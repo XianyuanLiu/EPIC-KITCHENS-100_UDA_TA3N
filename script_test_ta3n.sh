676039b47711ea750362e7f010c70960867b900f
Xianyuan <xianyuan.liu@sheffield.ac.uk>
2021-03-10 16:27:43 +0000

    Change to val

--- script_test_ta3n.sh
+++ script_test_ta3n.sh
@@ -36,7 +36,7 @@ if [ "$dataset" == "epic" ]
 then
 	dataset_source="source_train" # depend on users
 	dataset_target="target_train" # depend on users
-	dataset_val="target_test" # _noun" # depend on users
+	dataset_val="target_val" # _noun" # depend on users
 	num_source=16115 # number of training data (source)
 	num_target=26115 # number of training data (target)
 
